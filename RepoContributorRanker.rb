3e32b0375a0fbc40c320385fba83bae4471df708
Oleksii Kartamyshev <893147+madkumamon@users.noreply.github.com>
2023-12-26 18:00:43 +0200

    Tidy up

--- RepoContributorRanker.rb
+++ RepoContributorRanker.rb
@@ -10,9 +10,8 @@ require_relative 'progress_bar'
 Dotenv.load
 
 # ------------------------------------------------------------------------------
-# First day solution without thread, class breaking, database solution and optimisations
-
-
+# First day solution without thread, class breaking, database and optimisations
+# ------------------------------------------------------------------------------
 # Developer Scorecard
 # Dev team wants to track who did the most work each week for bragging rights. Using the GitHub API, create a maintainable API or interface that exposes a scorecard that shows the top contributors on a single GitHub repository in the past week. Assume the team uses GitHub flow and that the team only cares about pull request related events.
 # Score guidelines:
